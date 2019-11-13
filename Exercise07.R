475ef9ddb20011f0c0a6d8d09c25d25c0175f84e
David Pribyl Pierdinock <dpribylp@nd.edu>
2019-11-13 15:40:33 -0500

    started part 2

--- Exercise07.R
+++ Exercise07.R
@@ -9,3 +9,30 @@ oddrows <- function(x){
   }
 }
 
+# returns # of observations of a given species
+
+speciesNumber <- function(sp){
+  sum(iris$Species==sp)
+}
+
+#returns a dataframe for flowers with a sepal width greater than inputed value
+#DOESN"T WORK
+
+sepWid <- function(x = 1){
+  n = sum(iris[,2]>x)
+  sepList <- iris[1:n,]
+  count = 1
+  i = 1
+  for(i in nrow(iris)){
+    if(iris[i,2]>x){
+      sepList[count,] <- iris[i,]
+      count = count + 1
+    }
+    i = i+1
+  }
+  return(sepList)
+}
+sepWid(3.5)
+
+
+
