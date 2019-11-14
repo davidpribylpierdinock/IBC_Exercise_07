b4a0ec89370fe1e57c9a1b15470139a31534a996
John Hlavin <jhlavin@nd.edu>
2019-11-14 13:01:01 -0500

    fixed 2.2 and completed 2.3

--- Exercise07.R
+++ Exercise07.R
@@ -1,3 +1,5 @@
+#iris is a preloaded dataframe in Rstudio
+
 #Returns odd rows of a given dataframe
 
 oddrows <- function(x){
@@ -15,24 +17,30 @@ speciesNumber <- function(sp){
   sum(iris$Species==sp)
 }
 
-#returns a dataframe for flowers with a sepal width greater than inputed value
-#DOESN"T WORK
+#returns a dataframe to the console for flowers with a sepal width greater than inputed value
 
 sepWid <- function(x = 1){
   n = sum(iris[,2]>x)
   sepList <- iris[1:n,]
   count = 1
-  i = 1
-  for(i in nrow(iris)){
+  for(i in 1:nrow(iris)){
     if(iris[i,2]>x){
       sepList[count,] <- iris[i,]
       count = count + 1
     }
-    i = i+1
   }
   return(sepList)
 }
-sepWid(3.5)
 
+#makes a dataframe in global environment from sepWid function (using 4 as an example width value)
+
+sepWid_Data <- sepWid(4)
+
+#returns .csv file for a specified species
+
+speciesData <- function(x){
+  spDat <- iris[iris$Species==x,]
+  write.csv(spDat, paste(x,".csv"), row.names = FALSE)
+}
 
 
